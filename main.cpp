95db5ad73d371c73dcb5601ce8aab9d917d3d682
filipStacjonarny <filipw2001@gmail.com>
2021-01-21 17:16:49 +0100

    dziala dodawanie

--- main.cpp
+++ main.cpp
@@ -4,8 +4,36 @@
 
 using namespace std;
 
-void dodajAkta(){
+struct dane {
+    char numerSprawy[50];
+    string typ;
+    string dataWszczecia;
+    string dataZakonczenia;
+    char oficerProwadzacy[256];
+    string oskarzony;
+    string poszkodowany;
+    string swiadek;
+};
 
+void dodajAkta(vector<dane> &baza) {
+    dane record;
+    cout << "Podaj numer sprawy: ";
+    cin >> record.numerSprawy;
+    cout << "Podaj typ sprawy: ";
+    cin >> record.typ;
+    cout << "Podaj date wszczecia (DD.MM.RRRR): ";
+    cin >> record.dataWszczecia;
+    cout << "Podaj date zakonczenia (DD.MM.RRRR): ";
+    cin >> record.dataZakonczenia;
+    cout << "Podaj oficera prowadzacego (ImieNazwisko): ";
+    cin >> record.oficerProwadzacy;
+    cout << "Podaj oskarzonego (ImieNazwisko): ";
+    cin >> record.oskarzony;
+    cout << "Podaj poszkodowanego (ImieNazwisko): ";
+    cin >> record.poszkodowany;
+    cout << "Podaj swiadka (ImieNazwisko): ";
+    cin >> record.swiadek;
+    baza.push_back(record);
 }
 
 void usunAkta(){
@@ -29,47 +57,46 @@ void wczytajPlik(){
 }
 
 int main() {
-    struct dane {
-        char numerSprawy[50];
-        string typ;
-        string dataWszczecia;
-        string dataZakonczenia;
-        char oficerProwadzacy[256];
-        string oskrazony;
-        string poszkodowany;
-        string swiadek;
-    };
 
-    vector<dane> dane1;
+
+    vector<dane> baza;
 
     int wybor;
+    do {
+        cout << "Witaj w programie do zarzadzania archiwum kryminalnym!\nCo chcesz zrobic? " << endl;
+        cout << "1 - Dodanie akt" << endl;
+        cout << "2 - Usuniecie akt" << endl;
+        cout << "3 - Zmiana danych akt" << endl;
+        cout << "4 - Wyszukiwanie/filtrowanie po numerze lub typie" << endl;
+        cout << "5 - " << endl;
+        cout << "6 - Zapisanie bazy danych na dysku w postaci pliku" << endl;
+        cout << "7 - Wczytanie zapisanej bazy danych ze wskazanego pliku" << endl;
+        cout << "0 - Wyjscie z programu" << endl;
+        cin >> wybor;
 
-    cout << "Witaj w programie do zarzadzania archiwum kryminalnym!\nCo chcesz zrobic? " << endl;
-    cout << "1 - Dodanie akt" << endl;
-    cout << "2 - Usuniecie akt" << endl;
-    cout << "3 - Zmiana danych akt" << endl;
-    cout << "4 - Wyszukiwanie/filtrowanie po numerze lub typie" << endl;
-    cout << "5 - " << endl;
-    cout << "6 - Zapisanie bazy danych na dysku w postaci pliku" << endl;
-    cout << "7 - Wczytanie zapisanej bazy danych ze wskazanego pliku" << endl;
-    cin >> wybor;
+        switch (wybor) {
+            case 1:
+                dodajAkta(baza);
+                break;
+            case 2:
+                usunAkta();
+                break;
+            case 3:
+                zmienDane();
+                break;
+            case 4:
+                wyszukaj();
+                break;
+            case 5:
+                break;
+            case 6:
+                zapiszPlik();
+                break;
+            case 7:
+                wczytajPlik();
+                break;
+        }
+    } while(wybor != 0);
 
-    switch (wybor){
-        case 1:
-            dodajAkta();
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
-        case 6:
-            break;
-        case 7:
-            break;
-    }
     return 0;
 }
